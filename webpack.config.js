fd075814e7cfaadb924d9cd8a4954022acdca20e
Mikhail Yanov <yanovmikhailco@ya.ru>
2022-06-08 23:47:06 +0200

    +

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpackClientConfig = require('./cfg/webpack.client.config');
-const webpackServerConfig = require('./cfg/webpack.server.config');
+const webpackClientConfig = require('./cfg/webpack.client.config')
+const webpackServerConfig = require('./cfg/webpack.server.config')
 
 module.exports = [
   webpackClientConfig,

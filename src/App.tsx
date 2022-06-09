93156e2d8d638a3ee7d20bd3a790c49972c26707
Mikhail Yanov <yanovmikhailco@ya.ru>
2022-06-09 09:24:25 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root'
+import * as React from 'react'
+
+function AppComponent() {
+  return (
+    <div>content</div>
+  )
+}
+
+export const App = hot(AppComponent)
